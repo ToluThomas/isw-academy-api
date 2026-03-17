858b529655ca88b36d218c4a27e888fc75297cbe
Olamide Ojo <olamide.ojo@interswitchng.com>
2026-03-17 09:20:23 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -5,12 +5,22 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  ActivityIndicator,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const client = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -23,15 +33,107 @@ function App() {
   );
 }
 
+type PostProps = {
+  id?: number;
+  userId?: number;
+  title: string;
+  body: string;
+};
+
+function PostItem({ title, body }: PostProps) {
+  return (
+    <View>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+    </View>
+  );
+}
+
+type AllPostsProps = {
+  loading: Boolean;
+  posts?: PostProps[];
+};
+
+function AllPosts({ loading, posts }: AllPostsProps) {
+  return loading ? (
+    <ActivityIndicator />
+  ) : (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostItem {...item} />}
+      contentContainerStyle={{ gap: 16 }}
+    />
+  );
+}
+
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const [posts, setPosts] = useState<PostProps[]>();
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  function getPostsWithAxios() {
+    client
+      .get('/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function getPostsWithAxiosError() {
+    client
+      .get('/lego')
+      .then(response => {
+        setPosts(response.data);
+        setError('');
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function getPostsWithThen() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(responseJson => {
+        setPosts(responseJson);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  async function getPostWithAsync() {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      const responseJson = await response.json();
+      setPosts(responseJson);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getPostsWithAxiosError();
+  }, []);
+
+  console.log('Posts: ', posts);
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      {error ? (
+        <Text>{error}</Text>
+      ) : (
+        <AllPosts loading={loading} posts={posts} />
+      )}
     </View>
   );
 }
