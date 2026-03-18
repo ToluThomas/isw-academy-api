f00c4609c9d0ee1a32171f7efa48762a5d1f4659
Tolu Thomas <tolulope.thomas@interswitchgroup.com>
2026-03-18 11:11:00 +0100

    Pull to refresh

--- src/components/organisms/AllPosts.tsx
+++ src/components/organisms/AllPosts.tsx
@@ -1,12 +1,46 @@
 import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 import PostItem, { TPostProps } from '../molecules/PostItem';
+import { useEffect, useState } from 'react';
+import { retrievePostsFromMMKV, savePostsInMMKV } from '../../helpers/api';
+import { getPostsWithAxios } from '../../helpers/api/posts';
 
-type TAllPostsProps = {
-  isLoading?: boolean;
-  posts?: TPostProps[];
-};
+export default function AllPosts() {
+  const [posts, setPosts] = useState<TPostProps[]>();
+  const [isLoading, setLoading] = useState<boolean>(false);
 
-export default function AllPosts({ isLoading, posts }: TAllPostsProps) {
+  function onFetchPosts(posts: TPostProps[]) {
+    setPosts(posts);
+    savePostsInMMKV(posts);
+  }
+
+  function stopLoading() {
+    setLoading(false);
+  }
+
+  function onRefresh() {
+    getPostsWithAxios(posts => {
+      onFetchPosts(posts);
+    }).finally(stopLoading);
+  }
+
+  useEffect(() => {
+    const storedPosts = retrievePostsFromMMKV();
+    if (storedPosts.length > 0) setPosts(storedPosts);
+    else onRefresh();
+  }, []);
+
+  // Fetching and saving posts with AsyncStorage
+  // useEffect(() => {
+  //   retrievePostsFromAsyncStorage().then(retrievedPosts => {
+  //     if (retrievedPosts.length > 0) {
+  //       setPosts(retrievedPosts);
+  //       setLoading(false);
+  //     } else {
+  //       getPostsWithAxios(setPosts);
+  //     }
+  //   });
+  // }, []);
+
   return isLoading ? (
     <ActivityIndicator />
   ) : (
@@ -14,6 +48,8 @@ export default function AllPosts({ isLoading, posts }: TAllPostsProps) {
       data={posts}
       renderItem={({ item }) => <PostItem {...item} />}
       contentContainerStyle={styles.list}
+      refreshing={isLoading}
+      onRefresh={onRefresh}
     />
   );
 }
--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from 'react';
-import { getPostsWithAxios } from '../../helpers/api/posts';
-import { TPostProps } from '../molecules/PostItem';
 import { StyleSheet, View } from 'react-native';
 import AllPosts from '../organisms/AllPosts';
-import { retrievePostsFromMMKV, savePostsInMMKV } from '../../helpers/api';
 
 export default function Home() {
-  const [posts, setPosts] = useState<TPostProps[]>();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  function onFetchPosts(posts: TPostProps[]) {
-    setPosts(posts);
-    savePostsInMMKV(posts);
-  }
-
-  function stopLoading() {
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    const storedPosts = retrievePostsFromMMKV();
-    if (storedPosts.length > 0) setPosts(storedPosts);
-    else getPostsWithAxios(onFetchPosts).finally(stopLoading);
-  }, []);
-
-  // Fetching and saving posts with AsyncStorage
-  // useEffect(() => {
-  //   retrievePostsFromAsyncStorage().then(retrievedPosts => {
-  //     if (retrievedPosts.length > 0) {
-  //       setPosts(retrievedPosts);
-  //       setLoading(false);
-  //     } else {
-  //       getPostsWithAxios(setPosts);
-  //     }
-  //   });
-  // }, []);
-
   return (
     <View style={styles.container}>
-      <AllPosts isLoading={loading} posts={posts} />
+      <AllPosts />
     </View>
   );
 }
