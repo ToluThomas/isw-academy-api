c1c017f145749f0e4b64bbe8ea5ed976f4e34d0d
Tolu Thomas <tolulope.thomas@interswitchgroup.com>
2026-04-30 11:34:26 +0100

    Ignore redux-persist actions

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -28,6 +28,12 @@ const persistedReducer = persistReducer(persistConfig, combinedReducers);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
